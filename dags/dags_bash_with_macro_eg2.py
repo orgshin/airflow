8b0af5976113b974ea19d19f604a6dcdbca4c3d4
orgshin <joohwan9023@naver.com>
2025-09-10 23:37:27 +0900

    Fix : version 3

--- dags/dags_bash_with_macro_eg2.py
+++ dags/dags_bash_with_macro_eg2.py
@@ -7,13 +7,23 @@ with DAG(
     dag_id="dags_bash_with_macro_eg2",
     schedule="10 0 * * 6#2",
     start_date=pendulum.datetime(2025, 9, 1, tz="Asia/Seoul"),
-    catchup=False
+    catchup=False,
+    default_view="graph",
 ) as dag:
     # START_DATE: 2주전 월요일, END_DATE: 2주전 토요일
     bash_task_2 = BashOperator(
-        task_id='bash_task_2',
-        env={'START_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds}}',
-             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds}}'
+        task_id="bash_task_2",
+        env={
+            "START_DATE": (
+                '{{ ((data_interval_end.in_timezone("Asia/Seoul") '
+                '- macros.dateutil.relativedelta.relativedelta(weeks=2))'
+                '.start_of("week")) | ds }}'
+            ),
+            "END_DATE": (
+                '{{ ((data_interval_end.in_timezone("Asia/Seoul") '
+                '- macros.dateutil.relativedelta.relativedelta(weeks=2))'
+                '.start_of("week").add(days=5)) | ds }}'
+            ),
         },
-        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"',
     )
