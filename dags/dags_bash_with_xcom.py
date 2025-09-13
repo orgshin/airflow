0841ca19da70b09b50f033d77632334c8a43cb5a
orgshin <joohwan9023@naver.com>
2025-09-13 22:09:15 +0900

    fix : xcom date

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -6,7 +6,7 @@ from airflow.sdk import DAG
 with DAG(
     dag_id="dags_bash_with_xcom",
     schedule="10 0 * * *",
-    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    start_date=pendulum.datetime(2025, 9, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     bash_push = BashOperator(
